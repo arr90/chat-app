9a54acdd95bd0bf42d802439cd93c8ba4e71af95
Arthur Rocha Reis <arthur.reis@radixeng.com.br>
2018-11-05 15:05:55 -0200

    add modulo para prevenir XSS nas trocas de mensagens

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express'); //tratativa das rotas
 const path = require('path'); //padrao do node
 const socketIO = require('socket.io');
+const xss = require('xss');
 
 const PORT = process.env.PORT || 5000;
 
@@ -37,8 +38,9 @@ io.sockets.on('connection', socket => { //toda vez que um cliente se conectar
 
     socket.on('sendMessage', data => {
         console.log(data);
+        data.message = xss(data.message, {});//filtro para prevenir o XSS
+        data.author  = xss(data.author, {});
         messages.push(data);
-        
         socket.broadcast.emit('receivedMessage', data);//evento para enviar a mensagem para todos os sockets conectados
         socket.emit('receivedMessage', data);//evento para enviar a mensagem para o próprio socket
     });
